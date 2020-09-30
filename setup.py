22e4ac5b961a91d2f9447e5d4e3475a175cc9139
Ramesh Veerusikku <rameshveerfeb27@gmail.com>
2020-09-30 05:48:22 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='visionlib',
+    version='1.0.0',
+    packages=[],
+    url='git+https://github.com/tfdeepnet/visionlib.git',
+    license='MIT',
+    author='Deepak',
+    author_email='',
+    description='pytorch cnn models for visualization'
+    #install_requires=get_install_requirements(INSTALL_REQUIRES, CHOOSE_INSTALL_REQUIRES)
+   
+)
