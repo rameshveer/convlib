383dc313ed7e45c8f9006d1af7eded2c29f32845
Ramesh Veerusikku <rameshveerfeb27@gmail.com>
2020-10-03 19:34:45 +0530

    Update dataloader.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,33 +1,34 @@
 from torchvision import datasets
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-class dataloader(DataLoader):
+def dataloader(size, workers, cuda):
 
-    def dataloader(size, workers, cuda):
-
-        trainset = datasets.CIFAR10(root='./data', train=True,
+    trainset = datasets.CIFAR10(root='./data', train=True,
                                                 download=True, transform=transforms.transform)
-        testset = datasets.CIFAR10(root='./data', train=False,
+    testset = datasets.CIFAR10(root='./data', train=False,
                                                download=True, transform=transforms.transform)
 
-        if cuda:
-            size = size
-        else:
-            size = size
+    if cuda:
+        size = size
+    else:
+        size = size
 
-        dataloader_args = dict(shuffle=True, batch_size=size, num_workers=workers)
+    dataloader_args = dict(shuffle=True, batch_size=size, num_workers=workers)
 
-        trainloader = torch.utils.data.DataLoader(trainset, **dataloader_args)
+    trainloader = torch.utils.data.DataLoader(trainset, **dataloader_args)
 
-        testloader = torch.utils.data.DataLoader(testset, **dataloader_args)
+    testloader = torch.utils.data.DataLoader(testset, **dataloader_args)
 
-        return trainloader, testloader
+    return trainloader, testloader
 
 
-    def imgshow(self):
-        img = img / 2 + 0.5  # unnormalize
-        npimg = img.numpy()
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+def imgshow(self)
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
